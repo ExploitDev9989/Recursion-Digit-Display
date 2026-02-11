75c10951df044c1226becc210c19b6e6ecbcbc8e
ExploitDev9989 <miguelquiroz9989@gmail.com>
2026-02-11 13:36:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace number_using_recursion
+{
+    internal class Program
+    {
+       
+        
+        static void Main(string[] args)
+        {
+            //input
+            Console.WriteLine("Enter desired numbers: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Clear();
+            //checking for 0 from the user
+            if (n == 0)
+                Console.Write("0");
+            else
+                diplayNumbers(n);
+        }
+        // starting the recursion to display every number
+        static void diplayNumbers(int n)
+        {
+            
+            {
+                if (n == 0)
+                    return;
+            }
+            // removing the last number 
+            diplayNumbers(n /10);
+            // getting the remainder and printing with a space
+            Console.Write( n % 10 + " ");
+
+        }
+    }
+}
